a60f7d6e3a1c017a9269ce93e64b47ea31f869c5
Han <gksrudtlr2@icloud.com>
2025-09-01 17:20:00 +0900

    Update 2Weeks 17~20 문제 풀이

--- 2Weeks/17.2636.cpp
+++ 2Weeks/17.2636.cpp
@@ -12,9 +12,7 @@ vector<vector<bool>> visited;
 int n, m, times, ret;
 
 int Print()
-{
-	//cout << endl;
-	int count{};
+{	int count{};
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < m; j++)
@@ -23,12 +21,8 @@ int Print()
 				board[i][j] = 0;
 			if (board[i][j] == 1)
 				count++;
-			//cout << board[i][j] << " ";
 		}
-		//cout << endl;
 	}
-
-	//cout << count << endl;
 	return count;
 }
 
@@ -77,3 +71,13 @@ int main()
 	cout << times << endl;
 	cout << ret << endl;
 }
+
+//치즈를 녹여야 되는 문제
+//DFS를 이용해 치즈를 바로 녹이도록 풀었엇지만 틀렸음
+//아마 치즈에 둘러쌓인 공기층이 무시되더라도 치즈 겉면에 공기층이 1개가 아닌 여러개로 나눠져 있을 때 문제가 생기는 듯 함
+//그래서 DFS에서는 현재 공기층에 접촉한 부분 중 치즈에 둘러 쌓여있지 않은 부분들 모두 DFS에 돌 수 있도록 while문 안에서 반복을 할 것임
+//또한 DFS에서 치즈를 바로 녹이는 것이 아닌 공기층인지, 치즈인지만 판단할 것이고, 공기층이면 재귀를, 치즈라면 visited의 현재 위치를 true로 방문처리를 해준다
+//이후 DFS가 모두 끝나면 횟수를 카운트해주고, Print 함수에서 판의 크기만큼 반복문을 돌면서 현재 위치의 visited가 true라면 판에서 치즈를 녹여준다
+//그렇지 않다면 판에서 치즈인 부분인지 판단해 치즈의 수를 카운트 하고, 함수가 끝나면 총 치즈의 갯수를 return해준다
+// 이 값을 temp 변수에 저장해주고, temp가 0이라면 while문을 탈출하고 치즈가 다 녹은 시간과 다 녹기 전 마지막 치즈의 갯수를 출력해준다
+//이때 마지막 치즈의 갯수는 temp에 Print 함수의 return값을 저장하기 전 ret에 temp를 저장해주어 temp가 0일때 마지막 치즈의 갯수를 미리 저장해준다
